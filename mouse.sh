529d7c898fbe7c37890186dda97720449a092aad
Rostislav Romashin <allseen@gmail.com>
2024-01-01 00:39:26 +0300

    Update mouse.sh

--- mouse.sh
+++ mouse.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Often bluetooth adapter lost connection with mouse
 # This script will restart bluetooth adapter
+# chmod 2755 /usr/sbin/rfkill 
 rfkill block bluetooth
 rfkill unblock bluetooth
